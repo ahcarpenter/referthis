9261bcf7631d6339377639d059b38b9b40ed9831
Andrew Carpenter <ahcarpenter11@transy.edu>
2012-06-29 20:31:26 -0400

    delete file

--- lib/initializers/smtp_settings.rb
+++
@@ -1,8 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :address        => 'smtp.gmail.com',
-  :port           => 587,
-  :authentication => :plain,
-  :domain         => 'mail.google.com',
-  :user_name      => 'adoptahydrantsyracuse@gmail.com',
-  :password       => 'Drewby!23'
-}
