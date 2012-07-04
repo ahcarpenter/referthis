0cf915bc7c1702d6d0c60079c64776a1dfef430a
Andrew Carpenter <ahcarpenter11@transy.edu>
2012-07-03 20:01:53 -0400

    update

--- lib/sms.rb
+++ lib/sms.rb
@@ -8,6 +8,6 @@ class SMS
     return number.to_s.gsub(/\D/, '')
   end
   def self.send_referral(referral, url, referrer_name, app_name)
-    @@account.sms.messages.create(:from => '+18599030353', :to => Referee.find(referral.referee_id).endpoint, :body => referrer_name + ' has referred you to ' + app_name + '. You might be interested in checking out the following: ' + url + '/' + Base64::encode64(referral.id.to_s).chop + '.')
+    @@account.sms.messages.create(:from => '+18599030353', :to => Referee.find(referral.referee_id).endpoint, :body => referrer_name + ' has referred you to ' + app_name + '! You might be interested in checking out the following: ' + url + '/' + Base64::encode64(referral.id.to_s).chop + '.')
   end
 end
