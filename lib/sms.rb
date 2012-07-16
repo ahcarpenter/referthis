5cc1b977122236c5cb060af53b53d7391b65917b
Andrew Carpenter <ahcarpenter11@transy.edu>
2012-07-16 10:59:37 -0400

    update

--- lib/sms.rb
+++ lib/sms.rb
@@ -1,3 +1,4 @@
+# http://www.ruby-forum.com/topic/73919
 class SMS
   @@account_sid = 'AC0b322d7367604e7a852a1d59193738a2'
   @@auth_token = 'c32bcf082cb7cee728a99832858db23b'
@@ -7,7 +8,8 @@ class SMS
   def self.sieve(number)
     return number.to_s.gsub(/\D/, '')
   end
-  def self.send_referral(referral, url, referrer_name, app_name)
-    @@account.sms.messages.create(:from => '+18599030353', :to => Referee.find(referral.referee_id).endpoint, :body => referrer_name + ' has referred you to ' + app_name + '! You might be interested in checking out the following: ' + url + '/' + Base64::encode64(referral.id.to_s).chop + '.')
+  def self.send_referral(referral, url, referrer_name, app_name, message='')
+    message.length == 0 ? body = referrer_name + ' has referred you to ' + app_name + '! You might be interested in checking out the following: ' + url + '/' + Base64::encode64(referral.id.to_s).chop + '.' : message.slice(0,160)
+    @@account.sms.messages.create(:from => '+18599030353', :to => Referee.find(referral.referee_id).endpoint, :body => body)
   end
 end
